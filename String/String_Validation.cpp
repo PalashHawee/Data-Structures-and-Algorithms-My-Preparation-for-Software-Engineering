5189a6cca6081e535fdd017516a04ddda53f7b3f
Palash Hawee <67676859+PalashHawee@users.noreply.github.com>
2021-05-13 19:57:23 +0530

    String Validation

---
+++ String/String_Validation.cpp
@@ -0,0 +1,27 @@
+//Validating a string
+#include<stdio.h>
+
+int valid(char *name)
+{
+	int i;
+	for(i=0;name[i]!='\0'; i++)
+	{
+		if(!(name[i]>=65&&name[i<=90])&&!(name[i]>=97&&name[i<=127])&&!(name[i]>=48&&name[i]<=57))
+		{
+			return 0;
+		}
+		return 1;
+	}
+}
+int main()
+{
+	char *name="Ani?231";
+	if(valid(name))
+	{
+		printf("Valid String");
+	}
+	else
+	{
+		printf("Not valid string");
+	}
+}
