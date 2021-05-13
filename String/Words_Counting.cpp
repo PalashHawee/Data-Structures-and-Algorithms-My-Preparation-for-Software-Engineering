2f0fdb354ddb6e466de8a0a969d8e37c9c6b368a
Palash Hawee <67676859+PalashHawee@users.noreply.github.com>
2021-05-13 19:37:51 +0530

    Counting Words

---
+++ String/Words_Counting.cpp
@@ -0,0 +1,16 @@
+//COUNTING WORDS IN A STRING
+#include<stdio.h>
+
+int main()
+{
+	char A[]="How are you";
+	int i, word=0;
+	for(i=0;A[i]!='\0';i++)
+	{
+		if(A[i]==' '&&A[i-1]==' ')
+		{
+			word++;
+		}
+	}
+	printf("%d", word+1);
+}
