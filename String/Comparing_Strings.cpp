e51ab86e49fd864d3d63ed761988bca9a76f3720
Palash Hawee <67676859+PalashHawee@users.noreply.github.com>
2021-05-14 12:49:07 +0530

    Strings Comparison

---
+++ String/Comparing_Strings.cpp
@@ -0,0 +1,28 @@
+//Comparing two strings
+#include<stdio.h>
+
+int main()
+{
+	char A[]="Painter";
+	char B[]="Painting";
+	int i,j;
+	for(i=0,j=0;A[i]!='\0'&&B[j]!='\0';i++,j++)
+	{
+		if(A[i]!=B[j])
+		{
+			break;
+		}
+		if(A[i]==B[j])
+		{
+			printf("Two strings are equal");
+		}
+		else if(A[i]<B[j])
+		{
+			printf("A is smaller");
+		}
+		else
+		{
+			printf("A is greater");
+		}
+	}
+}
